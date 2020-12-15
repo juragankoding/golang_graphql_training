5c1d28a13335b800139f90a9c6dae9eb64b177f3
juragankoding <juragankoding@localhost.juragankoding>
2020-12-16 00:14:03 +0700

    udpate oder-item

---
+++ order_item/repository/order_item_repo.go
@@ -0,0 +1,37 @@
+package repository
+
+import (
+	"database/sql"
+
+	"github.com/juragankoding/golang_graphql_training/domain"
+)
+
+type orderItemRepository struct {
+	Conn *sql.DB
+}
+
+func NewGenerateOrderItem(Conn *sql.DB) domain.OrderItemRepository {
+	return &orderItemRepository{
+		Conn: Conn,
+	}
+}
+
+func (o *orderItemRepository) All() ([]*domain.OrderItem, error) {
+	return nil, nil
+}
+
+func (o *orderItemRepository) Single(id int) (*domain.OrderItem, error) {
+	return nil, nil
+}
+
+func (o *orderItemRepository) Insert(orderItem domain.OrderItem) (int64, error) {
+	return -1, nil
+}
+
+func (o *orderItemRepository) Update(orderItem domain.OrderItem) (int64, error) {
+	return -1, nil
+}
+
+func (o *orderItemRepository) Delete(id int) (int64, error) {
+	return -1, nil
+}
---
+++ order_item/usecase/order_item_usecase.go
@@ -0,0 +1,33 @@
+package usecase
+
+import "github.com/juragankoding/golang_graphql_training/domain"
+
+type orderItemUseCase struct {
+	orderItemRepository domain.OrderItemRepository
+}
+
+func NewGenerateOderItemRepository(orderItemRepository domain.OrderItemRepository) domain.OrderItemUseCase {
+	return &orderItemUseCase{
+		orderItemRepository: orderItemRepository,
+	}
+}
+
+func (o *orderItemUseCase) All() ([]*domain.OrderItem, error) {
+	return nil, nil
+}
+
+func (o *orderItemUseCase) Single(id int) (*domain.OrderItem, error) {
+	return nil, nil
+}
+
+func (o *orderItemUseCase) Insert(orderItem domain.OrderItem) (int64, error) {
+	return -1, nil
+}
+
+func (o *orderItemUseCase) Update(orderItem domain.OrderItem) (int64, error) {
+	return -1, nil
+}
+
+func (o *orderItemUseCase) Delete(id int) (int64, error) {
+	return -1, nil
+}
