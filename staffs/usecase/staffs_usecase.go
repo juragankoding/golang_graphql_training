8ff6c7ff6f061d45e8c12e995f2596536addc34d
juragankoding <juragankoding@localhost.juragankoding>
2020-12-15 19:03:12 +0700

    update staffs repository dan usecase dan perbaikan di schema agar sama

---
+++ staffs/usecase/staffs_usecase.go
@@ -0,0 +1,35 @@
+package usecase
+
+import (
+	"github.com/juragankoding/golang_graphql_training/domain"
+)
+
+type staffsUseCase struct {
+	StaffsRepo domain.StaffsRepository
+}
+
+func NewStaffsUseCase(staffRepo domain.StaffsRepository) domain.StaffsUseCase {
+	return &staffsUseCase{
+		StaffsRepo: staffRepo,
+	}
+}
+
+func (s *staffsUseCase) All() ([]*domain.Staffs, error) {
+	return nil, nil
+}
+
+func (s *staffsUseCase) Single(id int) (*domain.Staffs, error) {
+	return nil, nil
+}
+
+func (s *staffsUseCase) Insert(staffs domain.Staffs) (int64, error) {
+	return -1, nil
+}
+
+func (s *staffsUseCase) Update(staffs domain.Staffs) (int64, error) {
+	return -1, nil
+}
+
+func (s *staffsUseCase) Delete(id int) (int64, error) {
+	return -1, nil
+}
